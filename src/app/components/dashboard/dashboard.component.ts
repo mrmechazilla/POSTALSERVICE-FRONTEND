ad5f068207b8dadafa9a155d1b84a8762c307593
youssef <mhamed.youssef.benyoussef@gmail.com>
2024-06-02 14:26:49 +0100

    integrated dashboards with chartjs library

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js/auto';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
+
+  constructor(){}
+
+  ngOnInit(): void {
+    this.renderBarChart();
+    this.renderPieChart();
+    this.renderLineChart();
+  }
+
+  renderBarChart(){
+    new Chart("bar-chart", {
+      type: 'bar',
+      data: {
+        labels: ['2020', '2021', '2022', '2023', '2024', '2025'],
+        datasets: [{
+          label: 'dataset',
+          data: [13, 15, 6, 17, 20, 10],
+          borderWidth: 1
+        }]
+      }
+    })
+  }
+
+  renderPieChart(){}
+
+  renderLineChart(){
+    new Chart("line-chart", {
+      type: 'line',
+      data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [
+          {
+            label: 'Dataset 1',
+            data: [65, 59, 80, 81, 56, 55, 40],
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 1
+          },
+          {
+            label: 'Dataset 2',
+            data: [28, 48, 40, 19, 86, 27, 90],
+            borderColor: 'rgba(54, 162, 235, 1)',
+            borderWidth: 1
+          }
+        ]
+      }
+    });
+  }
 
 }
